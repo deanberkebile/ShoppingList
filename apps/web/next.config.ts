9a0c49bb1feb34c13182df81fea8f424d937d0d6
deanberkebile <deanberkebile@yahoo.com>
2026-05-07 21:06:27 -0400

    fixing web build

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,9 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.40.30'],
+  turbopack: {
+    root: path.join(__dirname, '../../'),
+  },
   ...(process.env.DOCKER_BUILD && {
     output: 'standalone',
     outputFileTracingRoot: path.join(__dirname, '../../'),
