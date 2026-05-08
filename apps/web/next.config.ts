f1d3f4d3217a35e587f4dcc3908030e6e79c36e2
deanberkebile <deanberkebile@yahoo.com>
2026-05-07 20:45:36 -0400

    adding missing files

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.40.30'],
+  ...(process.env.DOCKER_BUILD && {
+    output: 'standalone',
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  }),
 };
 
 export default nextConfig;
